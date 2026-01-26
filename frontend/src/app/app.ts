031e493808547ed85f0a552971b339eeecb84561
Marius Birk <52627911+mbirk1@users.noreply.github.com>
2026-01-26 18:19:02 +0100

    header and tailwind setup

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcome } from './nx-welcome';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [RouterModule, HeaderComponent],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
---
+++ frontend/src/app/components/header/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+})
+export class HeaderComponent {
+}
