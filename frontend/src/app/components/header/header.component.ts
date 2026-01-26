13072c6b3ab5c733cd682081af9290003275ac45
Marius Birk <52627911+mbirk1@users.noreply.github.com>
2026-01-26 19:14:34 +0100

    header and tailwind setup

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatToolbar, MatToolbarRow } from '@angular/material/toolbar';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatIconButton } from '@angular/material/button';
 
 @Component({
   imports: [],
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {
-}
+export class HeaderComponent {}
