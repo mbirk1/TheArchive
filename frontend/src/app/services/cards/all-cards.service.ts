a94d81a472e99d8558c3a85b9f86b5626614ac29
Marius Birk <52627911+mbirk1@users.noreply.github.com>
2026-02-16 19:26:11 +0100

    run prettier

--- frontend/src/app/services/cards/all-cards.service.ts
+++ frontend/src/app/services/cards/all-cards.service.ts
@@ -2,10 +2,9 @@ import { CardsStore } from '../../api/cards/cards.store';
 import { inject } from '@angular/core';
 import { Injectable } from '@angular/core';
 
-
 @Injectable({ providedIn: 'root' })
 export class AllCardsService {
-  private cardStore = inject(CardsStore)
+  private cardStore = inject(CardsStore);
 
   getAllCards() {
     return this.cardStore.allCards();
