e150d650b8a1c70e414ff33ec9928f930b3bbff2
Marius Birk <52627911+mbirk1@users.noreply.github.com>
2026-05-28 20:43:55 +0200

    fix build

--- backend/src/app/database/migrations/index.ts
+++ backend/src/app/database/migrations/index.ts
@@ -1,8 +1,6 @@
 import { MixedList } from 'typeorm';
-import { Migration1776974964877 } from './1776974964877-migration';
-import { Migration1778364681598 } from './1778364681598-migration';
+import { Migration1779993617040 } from './1779993617040-migration';
 
 export const allMigrations: MixedList<string | Function> = [
-  Migration1776974964877,
-  Migration1778364681598,
+  Migration1779993617040
 ];
