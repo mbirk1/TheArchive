12c6a81b4598d533fe32f672d59e7de83b11bb3f
Marius Birk <52627911+mbirk1@users.noreply.github.com>
2026-05-28 21:11:37 +0200

    fix build

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -30,9 +30,7 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const globalPrefix = 'api';
     app.setGlobalPrefix(globalPrefix);
-    app.enableCors({
-      origin: 'http://localhost:4200',
-    });
+    app.enableCors();
     const port = process.env.PORT || 3000;
     await app.listen(port);
 
