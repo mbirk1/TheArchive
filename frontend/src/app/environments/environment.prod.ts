9078742d616f2491395e92b77313780846977150
Marius Birk <52627911+mbirk1@users.noreply.github.com>
2026-05-30 22:25:44 +0200

    figuring port

--- frontend/src/app/environments/environment.prod.ts
+++ frontend/src/app/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'http://192.168.0.206:8084',
+  apiUrl: 'http://192.168.0.206',
   production: true,
   envName: 'prod',
   githubLink: 'https://github.com/mbirk1/TheArchive',
